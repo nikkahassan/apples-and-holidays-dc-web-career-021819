1fcedb5b92eb6e87faec753e49e8abbefe27c078
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-09 01:59:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,7 +22,10 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  
+ holiday_hash[:winter].each do |holiday, decorations|
+   decorations << supply
+  end
+end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
